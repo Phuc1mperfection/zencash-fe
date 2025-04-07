f91c070f36c86fdbbedf14737ae4d785fdc84dba
Phuc1mperfection <phuc0200366@huce.edu.vn>
2025-04-07 12:39:11 +0700

    feat(env): add .env file for environment variables and configure dotenv in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  define: {
+    'process.env': process.env,
   },
 });
