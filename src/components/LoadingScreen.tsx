8ad787aa53f02fb8f35d5b4522116285109e4757
Phuc1mperfection <phuc0200366@huce.edu.vn>
2025-05-18 17:35:27 +0700

    fix: adjust position of inner ring in LoadingScreen component for consistency

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,4 +1,3 @@
-
 const LoadingScreen = () => {
   return (
     <div className="fixed inset-0 bg-gradient-to-b from-[#001e2b] to-[#023430] flex items-center justify-center">
@@ -7,7 +6,7 @@ const LoadingScreen = () => {
         <div className="w-24 h-24 rounded-full border-4 border-white/20 animate-pulse"></div>
 
         {/* Inner Ring */}
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-16 h-16 rounded-full border-4 border-[#00ed64] animate-spin border-t-transparent"></div>
+        <div className="absolute left-1/2 top-1/2 w-16 h-16 rounded-full border-4 border-[#00ed64] animate-spin border-t-transparent"></div>
 
         {/* Center Dot */}
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-4 h-4 bg-[#00ed64] rounded-full animate-pulse"></div>
